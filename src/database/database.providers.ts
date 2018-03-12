a3d2a5b3cc1aba9cbe40a79ca08909caf55cc589
Dominik Kotecki <dominikus1910@gmail.com>
2018-03-12 18:59:02 +0100

    add typeorm

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,10 +6,10 @@ export const databaseProviders = [
         provide: "DbConnectionToken",
         useFactory: async () => await createConnection({
             type: "postgres",
-            host: "db",
-            port: parseInt(process.env.POSTGRES_PORT, 10),
-            username: process.env.POSTGRES_USERNAME,
-            password: process.env.POSTGRES_PASSWORD
+            host: process.env.POSTGRES_HOST || "db",
+            port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+            username: process.env.POSTGRES_USERNAME || "postgres",
+            password: process.env.POSTGRES_PASSWORD || "postgres"
         })
     }
 ];
